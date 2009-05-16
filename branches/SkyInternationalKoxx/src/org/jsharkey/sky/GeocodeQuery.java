81d3b7f013b15ad8f5ecdb77dafda912032a8e6f
koxx33 <koxx33@af711dd8-30ab-11de-9cad-a3574e5575ef>
2009-05-16 17:28:49 +0000

    functionnalities added : - choice for auto refreshing geolocation on update - manual update request possible - disable auto-update - correct refresh period bug

---
+++ branches/SkyInternationalKoxx/src/org/jsharkey/sky/GeocodeQuery.java
@@ -0,0 +1,47 @@
+package org.jsharkey.sky;
+
+import android.location.Address;
+import android.location.Location;
+
+public class GeocodeQuery {
+
+	String name = null;
+
+	double lat = Double.NaN;
+	double lon = Double.NaN;
+
+	public GeocodeQuery(String query) {
+		name = query;
+	}
+
+	public GeocodeQuery(Location location) {
+		lat = location.getLatitude();
+		lon = location.getLongitude();
+	}
+
+	/**
+	 * Summarize details of the given {@link Address}, walking down a
+	 * prioritized list of names until valid text is found to describe it.
+	 */
+	public GeocodeQuery(Address address) {
+		name = address.getLocality();
+		if (name == null) {
+			name = address.getFeatureName();
+		}
+		if (name == null) {
+			name = address.getAdminArea();
+		}
+		if (name == null) {
+			name = address.getPostalCode();
+		}
+		if (name == null) {
+			name = address.getCountryName();
+		}
+
+		// Fill in coordinates, if given
+		if (address.hasLatitude() && address.hasLongitude()) {
+			lat = address.getLatitude();
+			lon = address.getLongitude();
+		}
+	}
+}
